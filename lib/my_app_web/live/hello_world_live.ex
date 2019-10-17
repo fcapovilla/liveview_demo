6dedbfbcf5aec1442122aabd9fac27834e7f2b13
Frédérick Capovilla <caposite@hotmail.com>
2019-10-16 23:00:13 -0400

    Hello WAQ

--- lib/my_app_web/live/hello_world_live.ex
+++ lib/my_app_web/live/hello_world_live.ex
@@ -10,7 +10,7 @@ defmodule MyAppWeb.HelloWorldLive do
 
   def mount(_session, socket) do
     Phoenix.PubSub.subscribe(MyApp.PubSub, "hello-world-topic")
-    {:ok, assign(socket, message: "Hello World!")}
+    {:ok, assign(socket, message: "Hello WAQ!")}
   end
 
   def handle_event("add_exclamation", %{"text" => text}, socket) do
